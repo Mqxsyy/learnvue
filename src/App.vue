6500ec9cd56c64e9ee63c5413d41b9a2aae84207
Mqxsyy <113341234+Mqxsyy@users.noreply.github.com>
2023-10-12 11:32:30 +0300

    vue lesson 1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script setup>
 import { ref } from "vue";
 const message = ref("Hello Vue!");
+const newItem = ref("");
+const items = ref(["apple", "milk", "bread"]);
+
+function Add() {
+	items.push(newItem);
+}
 </script>
 
 <template>
-	<button @:click="message += 1">Click Me</button>
-	<input type="text" v-model="message" />
+	<button @:click="Add">Click Me</button>
+	<input type="text" v-model="newItem" />
 	<h1>{{ message.split("").reverse().join("") }}</h1>
+	<ul>
+		<li v-for="item in items">{{ item }}</li>
+	</ul>
 </template>
