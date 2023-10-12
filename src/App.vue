be379c9178af42406f2eaac7bb59a79a5a9aa432
Mqxsyy <113341234+Mqxsyy@users.noreply.github.com>
2023-10-12 11:25:16 +0300

    trying things

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const message = ref("Hello Vue!");
+</script>
 
-<template></template>
+<template>
+	<button @:click="message += 1">Click Me</button>
+	<input type="text" v-model="message" />
+	<h1>{{ message.split("").reverse().join("") }}</h1>
+</template>
