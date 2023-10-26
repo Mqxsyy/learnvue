68ecd2cc16366b55a75e8c2aab2789c1b31542ae
Mqxsyy <113341234+Mqxsyy@users.noreply.github.com>
2023-10-26 10:21:06 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <script setup>
-import { ref } from "vue";
+import { reactive, ref } from "vue";
+
 const message = ref("Hello Vue!");
-const newItem = ref("");
-const items = ref(["apple", "milk", "bread"]);
+const items = reactive(["apple", "milk", "bread"]);
 
 function Add() {
-	items.push(newItem);
+	items.push(message.value);
 }
 </script>
 
 <template>
 	<button @:click="Add">Click Me</button>
-	<input type="text" v-model="newItem" />
+	<input type="text" v-model="message" />
 	<h1>{{ message.split("").reverse().join("") }}</h1>
 	<ul>
 		<li v-for="item in items">{{ item }}</li>
