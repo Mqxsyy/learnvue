b3eccf480f830b3bcbb5ee7399c09b35e8ef197f
Mqxsyy <113341234+Mqxsyy@users.noreply.github.com>
2023-10-26 11:38:07 +0300

    Added bulma

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script setup>
-import { reactive, ref } from "vue";
+import { computed, reactive, ref } from "vue";
+import ItemList from "./components/ItemList.vue";
 
 const message = ref("Hello Vue!");
-const items = reactive(["apple", "milk", "bread"]);
+
+let i = 0;
+const items = reactive([
+	{ id: i++, name: "apple", isDone: true },
+	{ id: i++, name: "milk", isDone: true },
+	{ id: i++, name: "bread", isDone: false },
+	{ id: i++, name: "cheese", isDone: true },
+	{ id: i++, name: "eggs", isDone: true },
+]);
+
+const doneItems = computed(() => items.filter((item) => item.isDone));
+const todoItems = computed(() => items.filter((item) => !item.isDone));
 
 function Add() {
-	items.push(message.value);
+	items.push({ id: i++, name: message.value, isDone: false });
 }
 </script>
 
@@ -13,7 +25,7 @@ function Add() {
 	<button @:click="Add">Click Me</button>
 	<input type="text" v-model="message" />
 	<h1>{{ message.split("").reverse().join("") }}</h1>
-	<ul>
-		<li v-for="item in items">{{ item }}</li>
-	</ul>
+	<ItemList :items="items" title="All items"></ItemList>
+	<ItemList :items="doneItems" title="Done items"></ItemList>
+	<ItemList :items="todoItems" title="Todo items"></ItemList>
 </template>
