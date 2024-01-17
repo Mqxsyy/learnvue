cb2d6ac330dbfd4f2e05362c51f9e5ff155c0c88
Mqxsyy <113341234+Mqxsyy@users.noreply.github.com>
2024-01-17 11:42:07 +0200

    chuckNorris

--- src/App.vue
+++ src/App.vue
@@ -4,5 +4,7 @@ import Tabs from "./components/Tabs.vue";
 
 <template>
 	<Tabs></Tabs>
-	<router-view></router-view>
+	<Suspense>
+		<router-view></router-view>
+	</Suspense>
 </template>
---
+++ src/pages/ChuckNorris.vue
@@ -0,0 +1,21 @@
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+
+let joke = ref("");
+let res = await axios.get("https://api.chucknorris.io/jokes/random");
+joke.value = res.data.value;
+// axios.get("https://api.chucknorris.io/jokes/random").then((res) => {
+// 	joke.value = res.data.value;
+// });
+</script>
+
+<template>
+	<div class="container">
+		<div class="content">
+			<h2>
+				{{ joke }}
+			</h2>
+		</div>
+	</div>
+</template>
