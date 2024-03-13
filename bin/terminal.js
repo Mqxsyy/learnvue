9a7fce9b4e25cd5a5e9120e141c1bfa7994d2779
Mqxsyy <113341234+Mqxsyy@users.noreply.github.com>
2024-03-13 11:00:44 +0200

    Comic scraper

--- bin/terminal.js
+++ bin/terminal.js
@@ -1,80 +1,32 @@
-import inquirer from "inquirer";
+import axios from "axios";
+import * as cheerio from "cheerio";
+import fs from "node:fs";
 
-inquirer
-	.prompt([
-		{
-			type: "input",
-			name: "name",
-			message: "What's your name?",
-		},
-		{
-			type: "number",
-			name: "age",
-			message: "How old are you?",
-		},
-		{
-			type: "confirm",
-			name: "isHuman",
-			message: "Are you a human?",
-		},
-		{
-			type: "list",
-			name: "food",
-			message: "What's your favorite food?",
-			choices: ["Pizza", "Burger", "Salad"],
-		},
-		{
-			type: "rawlist",
-			name: "favoriteFruit",
-			message: "What's your favorite fruit?",
-			choices: ["Apple", "Orange", "Banana"],
-		},
-		{
-			type: "expand",
-			name: "mealPreference",
-			message: "What time of day do you prefer to eat?",
-			choices: [
-				{
-					key: "m",
-					name: "Morning",
-					value: "morning",
-				},
-				{
-					key: "a",
-					name: "Afternoon",
-					value: "afternoon",
-				},
-				{
-					key: "e",
-					name: "Evening",
-					value: "evening",
-				},
-			],
-		},
-		{
-			type: "checkbox",
-			name: "hobbies",
-			message: "What are your hobbies?",
-			choices: ["Reading", "Coding", "Gaming"],
-		},
-		{
-			type: "password",
-			name: "password",
-			message: "Enter your password",
-		},
-		{
-			type: "editor",
-			name: "bio",
-			message: "Write your bio",
-		},
-	])
-	.then((answers) => {
-		console.log(answers);
-	})
-	.catch((error) => {
-		if (error.isTtyError) {
-			// Prompt couldn't be rendered in the current environment
-		} else {
-			// Something else went wrong
-		}
-	});
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+
+if (!fs.existsSync("cache")) {
+	fs.mkdirSync("cache");
+}
+
+for (let i = 2905; i > 2895; i--) {
+	let cacheName = `cache/${i}.html`;
+	let data;
+
+	if (!fs.existsSync(cacheName)) {
+		await sleep(1000);
+
+		let res = await axios.get(`https://xkcd.com/${i}/`);
+		data = res.data;
+
+		fs.writeFileSync(cacheName, data);
+	} else {
+		data = fs.readFileSync(cacheName);
+	}
+
+	const $ = cheerio.load(data);
+	let img = $("div#comic>img");
+
+	console.log(img.attr("src"));
+	console.log(img.attr("title"));
+	console.log(img.attr("alt"));
+}
